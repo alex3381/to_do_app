e2dea9402e79e59617df5ae7420f5c9436e31451
fatogun <59268114+fatogun@users.noreply.github.com>
2021-04-29 14:19:16 +0100

    'to_do_app'

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,97 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/models/global.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            color: Colors.yellow,
+            home: SafeArea(
+                child: DefaultTabController(
+              length: 3,
+              child: new Scaffold(
+                body: Stack(children: [
+                  TabBarView(
+                    children: [
+                      new Container(
+                        color: darkGreyColor,
+                      ),
+                      new Container(
+                        color: Colors.orange,
+                      ),
+                      new Container(
+                        color: Colors.lightGreen,
+                      ),
+                      // new Container(
+                      //   color: Colors.red,
+                      // ),
+                    ],
+                  ),
+                  Container(
+                      padding: EdgeInsets.only(left: 50),
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(50)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Intray",
+                            style: intrayTitleStyle,
+                          ),
+                          Container()
+                        ],
+                      )),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    margin: EdgeInsets.only(
+                      top: 132,
+                      left: MediaQuery.of(context).size.width * 0.4 - 5,
+                    ),
+                    child: FloatingActionButton(
+                      child: Icon(
+                        Icons.add,
+                        size: 60,
+                      ),
+                      backgroundColor: redColor,
+                      onPressed: () {},
+                    ),
+                  )
+                ]),
+                appBar: AppBar(
+                  elevation: 0,
+                  title: new TabBar(
+                    tabs: [
+                      Tab(
+                        icon: new Icon(Icons.home),
+                      ),
+                      Tab(
+                        icon: new Icon(Icons.rss_feed),
+                      ),
+                      Tab(
+                        icon: new Icon(Icons.perm_identity),
+                      ),
+                    ],
+                    labelColor: darkGreyColor,
+                    unselectedLabelColor: Colors.blue,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicatorPadding: EdgeInsets.all(5.0),
+                    // indicatorColor: Colors.red,/
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+                backgroundColor: Colors.white,
+              ),
+            ))));
   }
 }
---
+++ lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF212128);
+Color redColor = new Color(0xFFDC4F64);
+TextStyle intrayTitleStyle = new TextStyle(
+  fontFamily: 'Avenir',
+  fontWeight: FontWeight.bold,
+  fontSize: 37,
+  color: Colors.black,
+);
